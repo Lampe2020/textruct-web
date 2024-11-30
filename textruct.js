0f61abf4bee5e67fddc918e3b463695f60c1e22c
Lampe2020 <Stehlampe2020@gmail.com>
2024-11-30 20:54:16 +0100

    Koden från livesändningen 2024-11-30

--- textruct.js
+++ textruct.js
@@ -1,8 +1,12 @@
+const conf = {
+    targetFPS: 60
+}
+
 // Initialisera spelets kod så tidigt som möjligt
-document.addEventListener('DOMContentLoaded', ()(=>{
+document.addEventListener('DOMContentLoaded', ()=>{
     const gameDisplayElement = document.querySelector('main');
     const gameDisplayDimensions = [0,0];
-    const buffers = {
+    const renderBuffers = {
         world: [],
         entities: [],
         ui: []
@@ -10,10 +14,21 @@ document.addEventListener('DOMContentLoaded', ()(=>{
 
     function render() {
         const screenBuffer = [];
-        for (var line=0; line<buffers.world.length; line++) {
-            screenBuffer[line] = [...buffers.world[line]];
+        for (var line=0; line<renderBuffers.world.length; line++) {
+            screenBuffer[line] = [...renderBuffers.world[line]];
+            for (var char=0; char<renderBuffers.entities.length; char++) {
+                if (renderBuffers.entities[line][char] !== '') {
+                    screenBuffer[line][char] = renderBuffers.entities[line][char];
+                }
+            }
+            for (var char=0; char<renderBuffers.ui.length; char++) {
+                if (renderBuffers.ui[line][char] !== '') {
+                    screenBuffer[line][char] = renderBuffers.ui[line][char];
+                }
+            }
         }
         console.log(screenBuffer); //debug
     }
-
+    
+    setInterval(render, 1000/conf.targetFPS); // Renderloop TODO: Gör om den med optionell väntetid i en traditionell loop för att tillåta lägre FPS när bilden inte kan rendras inom den efterfrågade tiden
 }, {once:true});
